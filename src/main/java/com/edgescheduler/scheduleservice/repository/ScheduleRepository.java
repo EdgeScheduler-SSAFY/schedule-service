e2920ad42e28e1c2933975a10c2f7d56489f033d
jep1995 <jep1995@naver.com>
2024-04-22 17:43:01 +0900

    ✨Feat :  dd schedule repository

--- src/main/java/com/edgescheduler/scheduleservice/repository/ScheduleRepository.java
+++ src/main/java/com/edgescheduler/scheduleservice/repository/ScheduleRepository.java
@@ -1,6 +1,11 @@
 package com.edgescheduler.scheduleservice.repository;
 
 import com.edgescheduler.scheduleservice.domain.Schedule;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {}
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    // 일정 상세 조회
+    public Optional<Schedule> findById(Long Id);
+}
