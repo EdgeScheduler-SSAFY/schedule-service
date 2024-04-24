6cba243aa0df85bdac7a7e2d0706caecdf131b36
jep1995 <jep1995@naver.com>
2024-04-24 17:17:12 +0900

    ♻️Refactor : remove ScheduleRepository code
    Change-Id: I145baf841d636998e5d7c27ef5a172059bb1862d

--- src/main/java/com/edgescheduler/scheduleservice/repository/ScheduleRepository.java
+++ src/main/java/com/edgescheduler/scheduleservice/repository/ScheduleRepository.java
@@ -1,11 +1,7 @@
 package com.edgescheduler.scheduleservice.repository;
 
 import com.edgescheduler.scheduleservice.domain.Schedule;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
-    // 일정 상세 조회
-    public Optional<Schedule> findById(Long Id);
 }
