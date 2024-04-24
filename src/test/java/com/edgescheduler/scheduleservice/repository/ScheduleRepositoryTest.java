f19bd1bcc24652f631e491dc984edf46e5f434f2
jep1995 <jep1995@naver.com>
2024-04-24 17:16:14 +0900

    ✨Feat :  add ScheduleRepositoryTest save code
    Change-Id: I0f7b578374bfdd79ceb5f8b210509a07f27c41f2

--- src/test/java/com/edgescheduler/scheduleservice/repository/ScheduleRepositoryTest.java
+++ src/test/java/com/edgescheduler/scheduleservice/repository/ScheduleRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.edgescheduler.scheduleservice.repository;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import com.edgescheduler.scheduleservice.domain.Attendee;
+import com.edgescheduler.scheduleservice.domain.AttendeeStatus;
 import com.edgescheduler.scheduleservice.domain.Schedule;
 import com.edgescheduler.scheduleservice.domain.ScheduleType;
 import java.time.Instant;
@@ -22,6 +25,9 @@ class ScheduleRepositoryTest {
     @Autowired
     private ScheduleRepository scheduleRepository;
 
+    @Autowired
+    private AttendeeRepository attendeeRepository;
+
     @DisplayName("일정 등록")
     @Test
     void save() {
@@ -36,9 +42,53 @@ class ScheduleRepositoryTest {
             .isPublic(true)
             .build();
 
-        scheduleRepository.save(schedule);
+        int member1Id = 1;
+        int member2Id = 2;
+        int member3Id = 3;
 
-        assertEquals(scheduleRepository.findAll().size(), 1);
+        Attendee attendee1 = Attendee.builder()
+            .schedule(schedule)
+            .memberId(member1Id)
+            .isRequired(true)
+            .status(AttendeeStatus.valueOf("PENDING"))
+            .build();
+
+        Attendee attendee2 = Attendee.builder()
+            .schedule(schedule)
+            .memberId(member2Id)
+            .isRequired(true)
+            .status(AttendeeStatus.valueOf("PENDING"))
+            .build();
+
+        Attendee attendee3 = Attendee.builder()
+            .schedule(schedule)
+            .memberId(member3Id)
+            .isRequired(true)
+            .status(AttendeeStatus.valueOf("PENDING"))
+            .build();
+
+        List<Attendee> attendees = List.of(attendee1, attendee2, attendee3);
+
+        schedule.setAttendees(attendees);
+
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+
+        Schedule scheduleRepeat = Schedule.builder()
+            .organizerId(1)
+            .name("일정명")
+            .description("일정 내용")
+            .type(ScheduleType.valueOf("MEETING"))
+            .startDatetime(Instant.parse("2024-04-24T11:00:00Z"))
+            .endDatetime(Instant.parse("2024-04-24T11:00:00Z"))
+            .googleCalendarId("googleCalendarId1")
+            .isPublic(true)
+            .build();
+
+        assertAll(
+            () -> assertEquals(3, savedSchedule.getAttendees().size()),
+            () -> assertEquals(1, scheduleRepository.findAll().size())
+        );
     }
 
     @DisplayName("일정 상세 조회")
