ec27cbea84362a6495291e3500552abfc099451c
오형택 <oh052679@gmail.com>
2024-04-25 16:45:50 +0900

    ✅Test :  add omitted test
    Change-Id: Ib8d5955a82c1fe2344d9f22244140617ff36e1bc

--- src/test/java/com/edgescheduler/scheduleservice/service/ScheduleMediateServiceTest.java
+++ src/test/java/com/edgescheduler/scheduleservice/service/ScheduleMediateServiceTest.java
@@ -1,6 +1,6 @@
 package com.edgescheduler.scheduleservice.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import com.edgescheduler.scheduleservice.domain.ScheduleType;
 import com.edgescheduler.scheduleservice.vo.ScheduleVO;
@@ -16,123 +16,121 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class ScheduleMediateServiceTest {
 
-  @Spy
-  private SimpleScheduleMediateService scheduleMediateService;
+    @Spy
+    private SimpleScheduleMediateService scheduleMediateService;
 
-  private static List<ScheduleVO> schedules;
+    private static List<ScheduleVO> schedules;
 
-  @BeforeAll
-  static void setUpSchedules() {
-    /**
-     *  2024-04-10 schedules
-     *  00:00 ~ 05:30 : PERSONAL
-     *  07:10 ~ 08:00 : PERSONAL
-     *  09:00 ~ 18:00 : WORKING     ** available time
-     *  11:00 ~ 13:30 : MEETING
-     *  19:30 ~ 20:30 : PERSONAL
-     *  23:30 ~ 24:00 : PERSONAL
-     */
-    schedules =
-        List.of(
-            new ScheduleVO(
-                1L,
-                "schedule1",
-                ScheduleType.PERSONAL,
-                Instant.parse("2024-04-09T23:30:00Z"),
-                Instant.parse("2024-04-10T05:30:00Z"),
-                true
-            ),
-            new ScheduleVO(
-                2L,
-                "schedule2",
-                ScheduleType.PERSONAL,
-                Instant.parse("2024-04-10T07:10:00Z"),
-                Instant.parse("2024-04-10T08:00:00Z"),
-                    true
-            ),
-            new ScheduleVO(
-                3L,
-                "schedule3",
-                ScheduleType.MEETING,
-                Instant.parse("2024-04-10T11:00:00Z"),
-                Instant.parse("2024-04-10T13:30:00Z"),
-                    true
-            ),
-            new ScheduleVO(
-                4L,
-                "schedule4",
-                ScheduleType.PERSONAL,
-                Instant.parse("2024-04-10T19:30:00Z"),
-                Instant.parse("2024-04-10T20:30:00Z"),
-                    true
-            ),
-            new ScheduleVO(
-                5L,
-                "schedule5",
-                ScheduleType.PERSONAL,
-                Instant.parse("2024-04-10T23:30:00Z"),
-                Instant.parse("2024-04-11T05:30:00Z"),
-                    true
-            ),
-            new ScheduleVO(
-                6L,
-                "schedule6",
-                ScheduleType.WORKING,
-                Instant.parse("2024-04-10T09:00:00Z"),
-                Instant.parse("2024-04-10T18:00:00Z"),
-                    true
-            )
-        );
-  }
-
-  @Test
-  void isAvailableWithOtherSchedule() {
-
-    List<Instant> timeTokens = new ArrayList<>();
-    Instant startTime = Instant.parse("2024-04-10T00:00:00Z");
-
-    for (int i = 0; i < 96; i++) {
-      timeTokens.add(startTime.plusSeconds(i * 900));
+    @BeforeAll
+    static void setUpSchedules() {
+        /**
+         *  2024-04-10 schedules
+         *  00:00 ~ 05:30 : PERSONAL
+         *  07:10 ~ 08:00 : PERSONAL
+         *  09:00 ~ 18:00 : WORKING     ** available time
+         *  11:00 ~ 13:30 : MEETING
+         *  19:30 ~ 20:30 : PERSONAL
+         *  23:30 ~ 24:00 : PERSONAL
+         */
+        schedules = List.of(new ScheduleVO(1L, "schedule1", ScheduleType.PERSONAL,
+                Instant.parse("2024-04-09T23:30:00Z"), Instant.parse("2024-04-10T05:30:00Z"), true),
+            new ScheduleVO(2L, "schedule2", ScheduleType.PERSONAL,
+                Instant.parse("2024-04-10T07:10:00Z"), Instant.parse("2024-04-10T08:00:00Z"), true),
+            new ScheduleVO(3L, "schedule3", ScheduleType.MEETING,
+                Instant.parse("2024-04-10T11:00:00Z"), Instant.parse("2024-04-10T13:30:00Z"), true),
+            new ScheduleVO(4L, "schedule4", ScheduleType.PERSONAL,
+                Instant.parse("2024-04-10T19:30:00Z"), Instant.parse("2024-04-10T20:30:00Z"), true),
+            new ScheduleVO(5L, "schedule5", ScheduleType.PERSONAL,
+                Instant.parse("2024-04-10T23:30:00Z"), Instant.parse("2024-04-11T05:30:00Z"), true),
+            new ScheduleVO(6L, "schedule6", ScheduleType.WORKING,
+                Instant.parse("2024-04-10T09:00:00Z"), Instant.parse("2024-04-10T18:00:00Z"),
+                true));
     }
 
-    Boolean[] expected = {
-        false, false, false, false,   // 00:00 ~ 01:00
-        false, false, false, false,   // 01:00 ~ 02:00
-        false, false, false, false,   // 02:00 ~ 03:00
-        false, false, false, false,   // 03:00 ~ 04:00
-        false, false, false, false,   // 04:00 ~ 05:00
-        false, false, true, true,     // 05:00 ~ 06:00
-        true, true, true, true,       // 06:00 ~ 07:00
-        false, false, false, false,   // 07:00 ~ 08:00
-        true, true, true, true,       // 08:00 ~ 09:00
-        true, true, true, true,       // 09:00 ~ 10:00
-        true, true, true, true,       // 10:00 ~ 11:00
-        false, false, false, false,   // 11:00 ~ 12:00
-        false, false, false, false,   // 12:00 ~ 13:00
-        false, false, true, true,     // 13:00 ~ 14:00
-        true, true, true, true,       // 14:00 ~ 15:00
-        true, true, true, true,       // 15:00 ~ 16:00
-        true, true, true, true,       // 16:00 ~ 17:00
-        true, true, true, true,       // 17:00 ~ 18:00
-        true, true, true, true,       // 18:00 ~ 19:00
-        true, true, false, false,     // 19:00 ~ 20:00
-        false, false, true, true,     // 20:00 ~ 21:00
-        true, true, true, true,       // 21:00 ~ 22:00
-        true, true, true, true,       // 22:00 ~ 23:00
-        true, true, false, false      // 23:00 ~ 24:00
-    };
+    @Test
+    void isAvailableWithOtherScheduleTest() {
 
-    var availabilities =
-        timeTokens.stream()
-            .map(
-                time ->
-                    scheduleMediateService.isAvailableWithOtherSchedule(
-                        time, time.plusSeconds(900), schedules))
-            .toArray();
+        List<Instant> timeTokens = new ArrayList<>();
+        Instant startTime = Instant.parse("2024-04-10T00:00:00Z");
 
-    assertArrayEquals(
-        expected,
-        availabilities
-    );
-  }
+        for (int i = 0; i < 96; i++) {
+            timeTokens.add(startTime.plusSeconds(i * 900));
+        }
+
+        Boolean[] expected = {false, false, false, false,   // 00:00 ~ 01:00
+            false, false, false, false,   // 01:00 ~ 02:00
+            false, false, false, false,   // 02:00 ~ 03:00
+            false, false, false, false,   // 03:00 ~ 04:00
+            false, false, false, false,   // 04:00 ~ 05:00
+            false, false, true, true,     // 05:00 ~ 06:00
+            true, true, true, true,       // 06:00 ~ 07:00
+            false, false, false, false,   // 07:00 ~ 08:00
+            true, true, true, true,       // 08:00 ~ 09:00
+            true, true, true, true,       // 09:00 ~ 10:00
+            true, true, true, true,       // 10:00 ~ 11:00
+            false, false, false, false,   // 11:00 ~ 12:00
+            false, false, false, false,   // 12:00 ~ 13:00
+            false, false, true, true,     // 13:00 ~ 14:00
+            true, true, true, true,       // 14:00 ~ 15:00
+            true, true, true, true,       // 15:00 ~ 16:00
+            true, true, true, true,       // 16:00 ~ 17:00
+            true, true, true, true,       // 17:00 ~ 18:00
+            true, true, true, true,       // 18:00 ~ 19:00
+            true, true, false, false,     // 19:00 ~ 20:00
+            false, false, true, true,     // 20:00 ~ 21:00
+            true, true, true, true,       // 21:00 ~ 22:00
+            true, true, true, true,       // 22:00 ~ 23:00
+            true, true, false, false      // 23:00 ~ 24:00
+        };
+
+        var availabilities = timeTokens.stream().map(
+            time -> scheduleMediateService.isAvailableWithOtherSchedule(time, time.plusSeconds(900),
+                schedules)).toArray();
+
+        assertArrayEquals(expected, availabilities);
+    }
+
+    @Test
+    void isOnWorkingHourAndAvailableTest() {
+
+        List<Instant> timeTokens = new ArrayList<>();
+        Instant startTime = Instant.parse("2024-04-10T00:00:00Z");
+
+        for (int i = 0; i < 96; i++) {
+            timeTokens.add(startTime.plusSeconds(i * 900));
+        }
+
+        Boolean[] expected = {
+            false, false, false, false,   // 00:00 ~ 01:00
+            false, false, false, false,   // 01:00 ~ 02:00
+            false, false, false, false,   // 02:00 ~ 03:00
+            false, false, false, false,   // 03:00 ~ 04:00
+            false, false, false, false,   // 04:00 ~ 05:00
+            false, false, false, false,   // 05:00 ~ 06:00
+            false, false, false, false,   // 06:00 ~ 07:00
+            false, false, false, false,   // 07:00 ~ 08:00
+            false, false, false, false,   // 08:00 ~ 09:00
+            true, true, true, true,       // 09:00 ~ 10:00
+            true, true, true, true,       // 10:00 ~ 11:00
+            false, false, false, false,   // 11:00 ~ 12:00
+            false, false, false, false,   // 12:00 ~ 13:00
+            false, false, true, true,     // 13:00 ~ 14:00
+            true, true, true, true,       // 14:00 ~ 15:00
+            true, true, true, true,       // 15:00 ~ 16:00
+            true, true, true, true,       // 16:00 ~ 17:00
+            true, true, true, true,       // 17:00 ~ 18:00
+            false, false, false, false,   // 18:00 ~ 19:00
+            false, false, false, false,   // 19:00 ~ 20:00
+            false, false, false, false,   // 20:00 ~ 21:00
+            false, false, false, false,   // 21:00 ~ 22:00
+            false, false, false, false,   // 22:00 ~ 23:00
+            false, false, false, false,   // 23:00 ~ 24:00
+        };
+
+        var availabilities = timeTokens.stream().map(
+            time -> scheduleMediateService.isOnWorkingHourAndAvailable(time, time.plusSeconds(900),
+                schedules)).toArray();
+
+        assertArrayEquals(expected, availabilities);
+    }
 }
--- src/test/java/com/edgescheduler/scheduleservice/service/SimpleScheduleServiceTest.java
+++ src/test/java/com/edgescheduler/scheduleservice/service/SimpleScheduleServiceTest.java
@@ -1,26 +1,183 @@
 package com.edgescheduler.scheduleservice.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.edgescheduler.scheduleservice.domain.ScheduleType;
+import com.edgescheduler.scheduleservice.dto.request.CalculateAvailabilityRequest;
+import com.edgescheduler.scheduleservice.dto.request.CalculateAvailabilityRequest.CalculatingMember;
 import com.edgescheduler.scheduleservice.repository.SimpleScheduleRepository;
+import com.edgescheduler.scheduleservice.vo.ScheduleVO;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 class SimpleScheduleServiceTest {
 
     @Mock
     private SimpleScheduleRepository scheduleRepository;
 
+    @Spy
+    private SimpleScheduleMediateService simpleScheduleMediateService;
+
     @InjectMocks
     private SimpleScheduleService simpleScheduleService;
 
-
-
     @Test
-    void calculateAvailability() {
+    void calculateAvailabilityTest() {
+
+        setUp();
+
+        List<int[]> expected = List.of(
+            new int[]{3, 2, 2, 2},
+            new int[]{3, 2, 2, 2},
+            new int[]{3, 2, 3, 2},
+            new int[]{4, 2, 4, 2},
+            new int[]{3, 1, 3, 1},
+            new int[]{3, 1, 3, 1},
+            new int[]{2, 0, 2, 0},
+            new int[]{2, 0, 2, 0},
+            new int[]{3, 1, 3, 1},
+            new int[]{3, 1, 3, 1},
+            new int[]{4, 2, 2, 1},
+            new int[]{4, 2, 2, 1}
+        );
+
+        var request = CalculateAvailabilityRequest.builder()
+            .startDatetime(LocalDateTime.of(2024, 4, 25, 10, 0))
+            .endDatetime(LocalDateTime.of(2024, 4, 25, 13, 0))
+            .memberList(
+                List.of(
+                    CalculatingMember.builder()
+                        .memberId(1)
+                        .isRequired(true)
+                        .build(),
+                    CalculatingMember.builder()
+                        .memberId(2)
+                        .isRequired(true)
+                        .build(),
+                    CalculatingMember.builder()
+                        .memberId(3)
+                        .isRequired(false)
+                        .build(),
+                    CalculatingMember.builder()
+                        .memberId(4)
+                        .isRequired(false)
+                        .build()
+                )
+            ).build();
+
+        var result = simpleScheduleService.calculateAvailability(request);
+
+        assertEquals(4, result.getSchedules().size());
+
+        AtomicInteger i = new AtomicInteger(0);
+        result.getTokenizedTimeAvailabilities()
+            .forEach(availability -> {
+                assertEquals(expected.get(i.get())[0], availability.getAvailableMemberCount());
+                assertEquals(expected.get(i.get())[1],
+                    availability.getAvailableRequiredMemberCount());
+                assertEquals(expected.get(i.get())[2],
+                    availability.getAvailableMemberInWorkingHourCount());
+                assertEquals(expected.get(i.getAndIncrement())[3],
+                    availability.getAvailableRequiredMemberInWorkingHourCount());
+            });
+    }
+
+    private void setUp() {
+
+        Instant startDatetime = Instant.parse("2024-04-25T10:00:00Z");
+        Instant endDatetime = Instant.parse("2024-04-25T13:00:00Z");
+
+        when(scheduleRepository.findByMemberIdAndEndDatetimeBeforeAndStartDatetimeAfter(
+            1, startDatetime, endDatetime
+        )).thenReturn(
+            List.of(
+                new ScheduleVO(
+                    1L,
+                    "schedule1",
+                    ScheduleType.WORKING,
+                    Instant.parse("2024-04-25T10:00:00Z"),
+                    Instant.parse("2024-04-25T13:00:00Z"),
+                    true
+                ),
+                new ScheduleVO(
+                    2L,
+                    "schedule2",
+                    ScheduleType.MEETING,
+                    Instant.parse("2024-04-25T11:00:00Z"),
+                    Instant.parse("2024-04-25T12:00:00Z"),
+                    true
+                )
+            )
+        );
+        when(scheduleRepository.findByMemberIdAndEndDatetimeBeforeAndStartDatetimeAfter(
+            2, startDatetime, endDatetime
+        )).thenReturn(
+            List.of(
+                new ScheduleVO(
+                    3L,
+                    "schedule3",
+                    ScheduleType.WORKING,
+                    Instant.parse("2024-04-25T09:00:00Z"),
+                    Instant.parse("2024-04-25T12:00:00Z"),
+                    true
+                ),
+                new ScheduleVO(
+                    4L,
+                    "schedule4",
+                    ScheduleType.PERSONAL,
+                    Instant.parse("2024-04-25T11:30:00Z"),
+                    Instant.parse("2024-04-25T12:30:00Z"),
+                    true
+                )
+            )
+        );
+        when(scheduleRepository.findByMemberIdAndEndDatetimeBeforeAndStartDatetimeAfter(
+            3, startDatetime, endDatetime
+        )).thenReturn(
+            List.of(
+                new ScheduleVO(
+                    5L,
+                    "schedule1",
+                    ScheduleType.WORKING,
+                    Instant.parse("2024-04-25T10:30:00Z"),
+                    Instant.parse("2024-04-25T13:00:00Z"),
+                    true
+                )
+            )
+        );
+        when(scheduleRepository.findByMemberIdAndEndDatetimeBeforeAndStartDatetimeAfter(
+            4, startDatetime, endDatetime
+        )).thenReturn(
+            List.of(
+                new ScheduleVO(
+                    6L,
+                    "schedule6",
+                    ScheduleType.WORKING,
+                    Instant.parse("2024-04-25T10:00:00Z"),
+                    Instant.parse("2024-04-25T12:30:00Z"),
+                    true
+                ),
+                new ScheduleVO(
+                    2L,
+                    "schedule2",
+                    ScheduleType.PERSONAL,
+                    Instant.parse("2024-04-25T10:00:00Z"),
+                    Instant.parse("2024-04-25T10:45:00Z"),
+                    true
+                )
+            )
+        );
     }
 }
