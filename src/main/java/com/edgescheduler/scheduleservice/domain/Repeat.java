c751d36c92238a8bef6e58699a23379cd6bd4e80
jep1995 <jep1995@naver.com>
2024-04-25 16:29:00 +0900

    ✨Feat : add Repeat entity
    Change-Id: Iac0b0110b347dac1afd52bd705b0d83a62b84aeb

--- src/main/java/com/edgescheduler/scheduleservice/domain/Repeat.java
+++ src/main/java/com/edgescheduler/scheduleservice/domain/Repeat.java
@@ -1,2 +1,40 @@
-package com.edgescheduler.scheduleservice.domain;public class Repeat {
+package com.edgescheduler.scheduleservice.domain;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Repeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private RepeatFreqType freq;
+
+    private int interval;
+
+    private Instant expiredDate;
+
+    private int count;
+
+    @Convert(converter = RepeatDaySetConverter.class)
+    private Set<String> repeatDay;
 }
