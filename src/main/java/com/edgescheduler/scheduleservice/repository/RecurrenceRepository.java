5d4ff2ed087251d39f00c0e36cc1ccb1b95f975a
jep1995 <jep1995@naver.com>
2024-05-02 14:42:54 +0900

    ✨Feat :  add repository
    Change-Id: Iad2933732ca1de8764c61a12f5c2c91859acb332

--- src/main/java/com/edgescheduler/scheduleservice/repository/RecurrenceRepository.java
+++ src/main/java/com/edgescheduler/scheduleservice/repository/RecurrenceRepository.java
@@ -1,2 +1,10 @@
-package com.edgescheduler.scheduleservice.repository;public interface RecurrenceRepository {
+package com.edgescheduler.scheduleservice.repository;
+
+import com.edgescheduler.scheduleservice.domain.Recurrence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecurrenceRepository extends JpaRepository<Recurrence, Long> {
+
 }
