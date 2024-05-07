79a166b717e4e2e75d003f78acbfef907e2fd5c5
오형택 <oh052679@gmail.com>
2024-05-07 17:08:22 +0900

    🐛Fix :  fix test api
    Change-Id: Ice3046982d44bffd69a837d95a2ea9a8d4e2cbae

--- src/main/java/com/edgescheduler/scheduleservice/controller/TestController.java
+++ src/main/java/com/edgescheduler/scheduleservice/controller/TestController.java
@@ -2,12 +2,11 @@ package com.edgescheduler.scheduleservice.controller;
 
 import com.edgescheduler.scheduleservice.message.MeetingCreateMessage;
 import com.edgescheduler.scheduleservice.service.KafkaProducer;
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.stream.IntStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +15,17 @@ public class TestController {
     private final KafkaProducer kafkaProducer;
 
     @PostMapping("/test")
-    public void test(){
+    public void test() {
         MeetingCreateMessage meetingCreateMessage = MeetingCreateMessage.builder()
-                .occurredAt(LocalDateTime.now())
-                .scheduleId(1L)
-                .scheduleName("Meeting")
-                .organizerId(2)
-                .organizerName("Organizer")
-                .startTime(LocalDateTime.now())
-                .endTime(LocalDateTime.now().plusHours(1))
-                .attendeeIds(IntStream.range(1, 100).boxed().toList())
-                .build();
+            .occurredAt(LocalDateTime.now())
+            .scheduleId(1L)
+            .scheduleName("Meeting")
+            .organizerId(2)
+            .organizerName("Organizer")
+            .startTime(LocalDateTime.now())
+            .endTime(LocalDateTime.now().plusHours(1))
+            .attendeeIds(IntStream.range(1, 6).boxed().toList())
+            .build();
         kafkaProducer.send("meeting-created", meetingCreateMessage);
     }
 }
